fd79791b5a57b9532fd46abdcacdd1a1bf47aeaf
Janne Leppänen <janne@janneleppanen.com>
2022-05-15 16:01:46 +0300

    feat: Add rotation to the car

--- src/car.ts
+++ src/car.ts
@@ -10,6 +10,7 @@ class Car {
   public acceleration: number = 0.2;
   public maxSpeed: number = 3;
   public friction: number = 0.95;
+  public angle: number = 0;
 
   public controls: Controls;
 
@@ -30,6 +31,17 @@ class Car {
       this.speed -= this.acceleration;
     }
 
+    if (this.speed !== 0) {
+      const flip = this.speed > 0 ? 1 : -1;
+
+      if (this.controls.left) {
+        this.angle += 0.03 * flip;
+      }
+      if (this.controls.right) {
+        this.angle -= 0.03 * flip;
+      }
+    }
+
     if (this.speed > 0) {
       this.speed = Math.min(this.speed, this.maxSpeed);
     } else {
@@ -38,18 +50,19 @@ class Car {
 
     this.speed *= this.friction;
 
-    this.y -= this.speed;
+    this.x -= Math.sin(this.angle) * this.speed;
+    this.y -= Math.cos(this.angle) * this.speed;
   }
 
   draw(ctx: CanvasRenderingContext2D) {
+    ctx.save();
+    ctx.translate(this.x, this.y);
+    ctx.rotate(-this.angle);
+
     ctx.beginPath();
-    ctx.rect(
-      this.x - this.width / 2,
-      this.y - this.height / 2,
-      this.width,
-      this.height
-    );
+    ctx.rect(-this.width / 2, -this.height / 2, this.width, this.height);
     ctx.fill();
+    ctx.restore();
   }
 }
 

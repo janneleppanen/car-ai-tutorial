3ce0b5963cadc0a092fe510300b8e4705c4bdd88
Janne Leppänen <janne@janneleppanen.com>
2022-05-15 15:40:08 +0300

    feat: Add controls to the car

--- src/car.ts
+++ src/car.ts
@@ -1,14 +1,28 @@
+import Controls from "./controls";
+
 class Car {
   public x: number;
   public y: number;
   public width: number;
   public height: number;
+  public controls: Controls;
 
   constructor(x: number, y: number, width: number, height: number) {
     this.x = x;
     this.y = y;
     this.width = width;
     this.height = height;
+
+    this.controls = new Controls();
+  }
+
+  update() {
+    if (this.controls.forward) {
+      this.y -= 2;
+    }
+    if (this.controls.reverse) {
+      this.y += 2;
+    }
   }
 
   draw(ctx: CanvasRenderingContext2D) {
---
+++ src/controls.ts
@@ -0,0 +1,52 @@
+class Controls {
+  public forward: boolean = false;
+  public left: boolean = false;
+  public right: boolean = false;
+  public reverse: boolean = false;
+
+  constructor() {
+    this.#addKeyboardListeners();
+  }
+
+  #addKeyboardListeners = () => {
+    document.onkeydown = (e: KeyboardEvent) => {
+      switch (e.key) {
+        case "ArrowUp":
+          this.forward = true;
+          break;
+        case "ArrowLeft":
+          this.left = true;
+          break;
+        case "ArrowRight":
+          this.right = true;
+          break;
+        case "ArrowDown":
+          this.reverse = true;
+          break;
+        default:
+          break;
+      }
+    };
+
+    document.onkeyup = (e: KeyboardEvent) => {
+      switch (e.key) {
+        case "ArrowUp":
+          this.forward = false;
+          break;
+        case "ArrowLeft":
+          this.left = false;
+          break;
+        case "ArrowRight":
+          this.right = false;
+          break;
+        case "ArrowDown":
+          this.reverse = false;
+          break;
+        default:
+          break;
+      }
+    };
+  };
+}
+
+export default Controls;

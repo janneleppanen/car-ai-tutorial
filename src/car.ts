af42a8d65792b5c8684b548fad5f1896dfd4f6a6
Janne Leppänen <janne@janneleppanen.com>
2022-05-22 14:33:20 +0300

    feat: Draw the car as a polygon

--- src/car.ts
+++ src/car.ts
@@ -16,6 +16,7 @@ class Car {
 
   public sensor: Sensor;
   public controls: Controls;
+  public polygon: Point[] = [];
 
   constructor(x: number, y: number, width: number, height: number) {
     this.x = x;
@@ -29,18 +30,19 @@ class Car {
 
   update(roadBorders: Point[][]) {
     this.move();
+    this.polygon = this.createPolygon();
     this.sensor.update(roadBorders);
   }
 
   draw(ctx: CanvasRenderingContext2D) {
-    ctx.save();
-    ctx.translate(this.x, this.y);
-    ctx.rotate(-this.angle);
-
     ctx.beginPath();
-    ctx.rect(-this.width / 2, -this.height / 2, this.width, this.height);
+    ctx.moveTo(this.polygon[0].x, this.polygon[0].y);
+
+    for (let i = 1; i < this.polygon.length; i++) {
+      ctx.lineTo(this.polygon[i].x, this.polygon[i].y);
+    }
+
     ctx.fill();
-    ctx.restore();
 
     this.sensor.draw(ctx);
   }
@@ -74,6 +76,34 @@ class Car {
 
     this.x -= Math.sin(this.angle) * this.speed;
     this.y -= Math.cos(this.angle) * this.speed;
+  }
+
+  private createPolygon() {
+    const points = [];
+    const rad = Math.hypot(this.width, this.height) / 2;
+    const alpha = Math.atan2(this.width, this.height);
+
+    points.push({
+      x: this.x - Math.sin(this.angle - alpha) * rad,
+      y: this.y - Math.cos(this.angle - alpha) * rad,
+    });
+
+    points.push({
+      x: this.x - Math.sin(this.angle + alpha) * rad,
+      y: this.y - Math.cos(this.angle + alpha) * rad,
+    });
+
+    points.push({
+      x: this.x - Math.sin(Math.PI + this.angle - alpha) * rad,
+      y: this.y - Math.cos(Math.PI + this.angle - alpha) * rad,
+    });
+
+    points.push({
+      x: this.x - Math.sin(Math.PI + this.angle + alpha) * rad,
+      y: this.y - Math.cos(Math.PI + this.angle + alpha) * rad,
+    });
+
+    return points;
   }
 }
 
